5b9cf517eabf10321dd395e3e6917bfba616254e
Nelsn <nelsnbro@gmail.com>
2024-02-17 22:35:59 +0300

    add links, add create url inline keyboard def

--- keyboards/inline_kb.py
+++ keyboards/inline_kb.py
@@ -1,6 +1,6 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from lexicon.lexicon import LEXICON
+from lexicon.lexicon import LEXICON, LINKS
 
 
 def get_markup(width, *args, **kwargs):
@@ -22,3 +22,18 @@ def get_markup(width, *args, **kwargs):
 
     kb_builder.row(*buttons, width=width)
     return kb_builder.as_markup()
+
+
+def get_markup_url(*args):
+    kb_buidler = InlineKeyboardBuilder()
+    buttons = []
+
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=button,
+                url=LINKS[button]
+            ))
+
+    kb_buidler.row(*buttons)
+    return kb_buidler.as_markup()
